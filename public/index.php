0c843351a2ec2e2c3beac38a3f88706fc6622e2c
Герасименко Алекс < >
2016-08-02 13:56:08 +0300

    Add logo and favicon

--- public/index.php
+++ public/index.php
@@ -1,28 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Gerasimenko Alexandr</title>
-    <style>
-        html, body {
-            height: 100%;
-        }
-
-        body {
-            background-color: #CFCD2F;
-            color: #406386;
-            font-family: monospace;
-        }
-
-        .main {
-            position: fixed;
-            top: 50%;
-            margin-top: -100px;
-            height: 200px;
-            text-align: center;
-            width: 100%
-        }
-    </style>
+    <title>HereUA company</title>
+    <link rel="shortcut icon" href="/images/favicon.png" type="image/png">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
 <section class="main">
