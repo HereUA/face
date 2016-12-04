c64bd0a2827897afa49bade80d81ec5b6d583761
GerasimenkoAlexander <gerasimenkoalexanderua@gmail.com>
2016-12-05 01:20:59 +0200

    Add nav and imgs

--- public/index.php
+++ public/index.php
@@ -2,14 +2,39 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>HereUA company</title>
     <link rel="shortcut icon" href="/images/favicon.png" type="image/png">
     <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
+<header>
+    <div>
+        <h1 id="logo">
+            <a href="/" title="HereUA">
+                <img src="/images/logo.png" alt="HereUA" />
+            </a>
+        </h1>
+    </div>
+    <div class="main-nav-wrap">
+        <nav class="main-nav">
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#service">Service</a></li>
+                <li><a href="#team">Team</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#contact-us">Contact Us</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
 <section class="main">
-    <h1>Here UA</h1>
-    <div>coming soon...</div>
+    <section class="content">
+        <section class="cover home-page" id="#home">
+
+        </section>
+    </section>
 </section>
 </body>
 </html>
