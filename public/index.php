ad3a3f4a228a3606caafffce5deeecea66ada992
GerasimenkoAlexander <gerasimenkoalexanderua@gmail.com>
2016-12-14 04:33:35 +0200

    Add bootstrap4a5

--- public/index.php
+++ public/index.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>HereUA company</title>
     <link rel="shortcut icon" href="/images/favicon.png" type="image/png">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="/css/bootstrap-reboot.min.css">
     <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
@@ -72,6 +76,7 @@
     integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
     crossorigin="anonymous">
 </script>
+<script src="/js/bootstrap.min.js"></script>
 <script src="/js/main.js"></script>
 </body>
 </html>
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,13 @@
 $(function(){
 
+    /*http://v4-alpha.getbootstrap.com/getting-started/browsers-devices/#select-menu*/
+    var nua = navigator.userAgent;
+    var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1)
+    if (isAndroid) {
+        $('select.form-control').removeClass('form-control').css('width', '100%')
+    }
+
+    /*menu*/
     var $document = $(document);
     var $nav = $('header');
     var $main = $('.main');
